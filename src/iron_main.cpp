1f3daa14c5d6c8f593dfb5ec79b42494d439b793
cn86-zhh <opensourceruler@163.com>
2026-03-23 11:13:55 +0800

    add new file

--- src/iron_main.cpp
+++ src/iron_main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <string>
 
-// #ifdef _WIN32
-// #include <windows.h>
-// #include <io.h>
-// #include <fcntl.h>
-// #else
-// #include <unistd.h>
-// #endif
-
 #include "iron_cross_platform.hpp"
 #include "iron_handle.hpp"
 #include "iron_server.hpp"
