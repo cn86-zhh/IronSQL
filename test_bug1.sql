d6dbfdcde84d8e9257cfaf3bbf1133fcd3d7c429
cn86-zhh <opensourceruler@163.com>
2026-03-29 17:19:15 +0800

    update a few file
    update file

--- test_bug1.sql
+++
@@ -1,9 +0,0 @@
--- 测试 Bug 1: drop table 后表数量计数器未更新
-show databases;
-use users;
-show tables;
-get current database; get tables number;
-drop table notifications;
-show tables;
-get current database; get tables number;
-exit;
